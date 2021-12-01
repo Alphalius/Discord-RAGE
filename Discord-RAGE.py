05863b951e527b8f2d6e9e2ce29ae28459699317
Alphalius <80674770+Alphalius@users.noreply.github.com>
2021-12-01 10:34:01 +0000

    Add files via upload

---
+++ Discord-RAGE.py
@@ -0,0 +1,45 @@
+# Imports the needed libaries
+import os
+import pyautogui
+import webbrowser
+import time
+
+# Changes the window title
+os.system("title Discord SPAM")
+
+# Prints the title
+image = ("""          
+          
+          ██████  ██ ███████  ██████  ██████  ██████  ██████      ██████   █████   ██████  ███████ 
+          ██   ██ ██ ██      ██      ██    ██ ██   ██ ██   ██     ██   ██ ██   ██ ██       ██      
+          ██   ██ ██ ███████ ██      ██    ██ ██████  ██   ██     ██████  ███████ ██   ███ █████   
+          ██   ██ ██      ██ ██      ██    ██ ██   ██ ██   ██     ██   ██ ██   ██ ██    ██ ██      
+          ██████  ██ ███████  ██████  ██████  ██   ██ ██████      ██   ██ ██   ██  ██████  ███████ 
+                                                                                                   
+                                                                                                   """)
+
+print(image)
+
+# Asks the user for the inputs
+message = input("Enter the message to spam: ")
+repeats = int(input("Enter the amount of times it needs to be send: "))
+delay = int(input("Enter the delay amount in MS: "))
+
+# Asks the user if Discord is loaded
+isLoaded = input("Press enter if Discord is loaded")
+
+# Sleeps for 5 seconds
+time.sleep(5)
+
+# Spams in Discord
+for i in range (0,repeats):
+    if message != "":
+         pyautogui.typewrite(message)
+         pyautogui.press("enter")
+    else:
+         pyautogui.hotkey('ctrl', 'v')
+         pyautogui.press("enter")
+    time.sleep(delay/1000)
+
+# Shows the user it's finished
+print("Done\n")
